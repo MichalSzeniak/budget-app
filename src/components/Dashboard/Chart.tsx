41de0380af625f1a085e7994028ac66f194b3f14
Michal <michal.szeniak@gmail.com>
2024-08-14 00:24:55 +0200

    feat: chart

---
+++ src/components/Dashboard/Chart.tsx
@@ -0,0 +1,155 @@
+import { Label, Pie, PieChart } from "recharts";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { useMemo } from "react";
+import { Transaction, TransactionsState } from "@/store/transactionsSlice";
+
+const chartConfig = {
+  health: {
+    label: "Health",
+    color: "hsl(var(--chart-health))", // Azure Blue
+  },
+  home: {
+    label: "Home",
+    color: "hsl(var(--chart-home))", // Greenish
+  },
+  leisure: {
+    label: "Leisure",
+    color: "hsl(var(--chart-leisure))", // Violet
+  },
+  education: {
+    label: "Education",
+    color: "hsl(var(--chart-education))", // Blueish
+  },
+  gifts: {
+    label: "Gifts",
+    color: "hsl(var(--chart-gifts))", // Pinkish
+  },
+  groceries: {
+    label: "Groceries",
+    color: "hsl(var(--chart-groceries))", // Green
+  },
+  family: {
+    label: "Family",
+    color: "hsl(var(--chart-family))", // Orange
+  },
+  workout: {
+    label: "Workout",
+    color: "hsl(var(--chart-workout))", // Red
+  },
+  transportation: {
+    label: "Transportation",
+    color: "hsl(var(--chart-transportation))", // Sky Blue
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-other))", // Gray
+  },
+} satisfies ChartConfig;
+
+interface ChartConfigItem {
+  label: string;
+  color: string;
+}
+
+interface ChartConfig {
+  [key: string]: ChartConfigItem;
+}
+
+interface ChartDataItem {
+  category: string;
+  amount: number;
+  fill: string;
+}
+
+const Chart = ({ transactions }: TransactionsState) => {
+  const useChartData = (
+    data: Transaction[],
+    chartConfig: ChartConfig
+  ): ChartDataItem[] => {
+    return useMemo(() => {
+      const aggregatedData = data.reduce(
+        (acc: { [key: string]: number }, item) => {
+          const category = item.category.toLowerCase();
+          acc[category] = (acc[category] || 0) + item.amount;
+          return acc;
+        },
+        {}
+      );
+
+      return Object.entries(aggregatedData).map(([category, amount]) => {
+        const configItem = chartConfig[category];
+        return {
+          category: configItem ? configItem.label : "Other",
+          amount,
+          fill: configItem ? configItem.color : chartConfig.other.color,
+        };
+      });
+    }, [data, chartConfig]);
+  };
+  const chartData = useChartData(transactions, chartConfig);
+
+  const totalAmount = useMemo(() => {
+    return chartData.reduce((acc, curr) => acc + curr.amount, 0);
+  }, [chartData]);
+
+  return (
+    <Card className="flex flex-col w-full">
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="amount"
+              nameKey="category"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-3xl font-bold"
+                        >
+                          {totalAmount.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          PLN
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
+export default Chart;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,19 +2,23 @@ import { selectTransactions } from "@/store/transactionsSlice";
 import { useSelector } from "react-redux";
 import EmptyBudget from "@/components/Dashboard/EmptyBudget";
 import DashboardTable from "@/components/Dashboard/DashboardTable";
+import Chart from "@/components/Dashboard/Chart";
 
 export function Dashboard() {
   const transactions = useSelector(selectTransactions);
 
   return (
-    <div className="flex h-full">
+    <div className="flex h-[calc(100%-60px)]">
       <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
         <div className="flex items-center">
           <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
         </div>
-        <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm p-10 sm:p2">
+        <div className="flex flex-col flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm p-10 sm:p2">
           {transactions.length ? (
-            <DashboardTable transactions={transactions} />
+            <>
+              <Chart transactions={transactions} />
+              <DashboardTable transactions={transactions} />
+            </>
           ) : (
             <EmptyBudget />
           )}
