43c43f0fdf2e398699f6a93c4dd05892c5e46eb5
Michal <michal.szeniak@gmail.com>
2024-08-19 04:31:56 +0200

    deploy test

--- src/components/TransactionForm/TransactionForm.tsx
+++ src/components/TransactionForm/TransactionForm.tsx
@@ -30,7 +30,7 @@ import {
   incomeCategoryList,
 } from "@/constants/categories";
 import { toast } from "../ui/use-toast";
-import { DatePicker } from "../ui/datePicker";
+import { DatePicker } from "../ui/DatePicker";
 
 interface TransactionFormProps {
   type: "expense" | "income";
